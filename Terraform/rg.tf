9112d5b0530cb7cb841462301cc5bb021e2cdf29
roh1234567 <157359771+roh1234567@users.noreply.github.com>
2024-04-07 19:56:02 +0530

    Update rg.tf

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "demo" {
-  name     = "example-resources"
+  name     = "Bluepi-resources"
   location = "CentralIndia"
 }
 
 ##  Demo now
 resource "azurerm_storage_account" "StorageAccountDemo" {
-  name                     = "satestant000012"
+  name                     = "Bluepi"
   resource_group_name      = azurerm_resource_group.demo.name
   location                 = azurerm_resource_group.demo.location
   account_tier             = "Standard"
