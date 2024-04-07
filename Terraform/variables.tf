a9a8ba22e21eb3eb62645928179c0bde246d4fa3
rohit <rohit.yadav@bluepi.in>
2024-04-07 14:58:41 +0530

    tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,34 @@
 data "azurerm_location" "current" {
   location = "CentralIndia"
 }
+
+variable "env" {
+  description = "Environment Name in small case"
+  type        = string
+}
+
+variable "COSTCENTER" {
+  description = "Cost Center Name"
+  type        = string
+}
+
+variable "DEPARTMENT" {
+  description = "Department Name"
+  type        = string
+}
+
+variable "OWNER" {
+  description = "Owner Name"
+  type        = string
+}
+
+variable "PROJECT" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "project" {
+  description = "Project Name in small case"
+  type        = string
+}
+
