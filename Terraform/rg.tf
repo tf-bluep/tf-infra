6827f280bdf4970a88be842a6cccf1016a90f7bf
rohit <rohit.yadav@bluepi.in>
2024-04-07 14:54:15 +0530

    tf

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -1,14 +1,13 @@
 
 module "azure_resourcegroup" {
   source  = "app.terraform.io/rohityad/azure_resourcegroup/azurerm"
-  version = "1.0.0
+  version = "1.0.0"
 
   application = "Bluepi"
   costcenter  = "abc"
-  department  = "abc"
+  department  = "xyz"
   env         = "dev"
   location    = data.azurerm_location.current.display_name
   owner       = "rohityad"
   rgname      = "RG-bluepi"
-
 }
