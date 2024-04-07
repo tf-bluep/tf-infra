5c56427ffa9a88f4dfb23f437bef478a0e0c6198
rohit <rohit.yadav@bluepi.in>
2024-04-07 14:51:16 +0530

    tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -10,4 +10,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = "f2fb6ada-1520-4d1a-a23b-8ebbcf63a24c"
+  client_id       = "id05bef3fb-6ca0-4af4-b445-6e64d672c7d6"
+  client_secret   = "e88Q~07ZraylA8ixVufA.szJxopm0NRhCzqEbZh"
+  tenant_id       = "23e4ff6d-7f45-4d40-8565-6135271fcd0c"
 }
