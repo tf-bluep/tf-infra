a3e459dcee71b5cdab57bd8c3f5c59903f2a6a18
roh1234567 <157359771+roh1234567@users.noreply.github.com>
2024-04-08 12:12:44 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,19 +1,19 @@
-terraform {
-  required_version = ">= 1.1.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~> 3.0.2"
-    }
-  }
-  cloud {
-    organization = "rohityad"
-    workspaces {
-      name = "alerts"
-    }
-  }
-}
+#terraform {
+ # required_version = ">= 1.1.0"
+  #required_providers {
+   # azurerm = {
+    #  source = "hashicorp/azurerm"
+     # version = "~> 3.0.2"
+    #}
+  #}
+  #cloud {
+  #  organization = "rohityad"
+  #  workspaces {
+   #   name = "alerts"
+   # }
+  #}
+#}
 
-provider "azurerm" {
-  features {}
-}
+#provider "azurerm" {
+ # features {}
+#}
