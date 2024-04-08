9328c83a0b44ce9e7e3451d34853b139ef179bc4
roh1234567 <157359771+roh1234567@users.noreply.github.com>
2024-04-08 11:14:28 +0530

    Update rg.tf

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -1,13 +1,2 @@
 
-module "azure_resourcegroup" {
-  source  = "app.terraform.io/rohityad/azure_resourcegroup/azurerm"
-  version = "1.0.0"
 
-  application = "Bluepi"
-  costcenter  = "abc"
-  department  = "xyz"
-  env         = "dev"
-  location    = data.azurerm_location.current.display_name
-  owner       = "rohityad"
-  rgname      = "RG-bluepi"
-}
