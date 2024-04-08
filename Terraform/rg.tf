61e5f04b1f6b07a15de82b4101b06937ba2c010a
roh1234567 <157359771+roh1234567@users.noreply.github.com>
2024-04-08 09:49:30 +0530

    Update rg.tf

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -13,3 +13,33 @@ resource "azurerm_storage_account" "StorageAccountDemo" {
 
 
 }
+resource "azurerm_monitor_action_group" "ActionGroupDemo" {
+  name                = "dev-team-action-group"
+  resource_group_name = azurerm_resource_group.demo.name
+  short_name          = "exactiongrp"
+  email_receiver {
+    name          = "email"
+    email_address = "rohit.yadav@bluepi.in"
+  }
+}
+
+resource "azurerm_monitor_metric_alert" "storage_account_alert" {
+  name                = "storage-account-capacity-alert"
+  resource_group_name = azurerm_resource_group.demo.name
+  scopes              = [azurerm_storage_account.StorageAccountDemo.id]
+
+  criteria {
+    metric_namespace = "Microsoft.Storage/storageAccounts"
+    metric_name      = "UsedCapacity"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 1
+  }
+
+  action {
+    action_group_id = azurerm_monitor_action_group.ActionGroupDemo.id
+  }
+
+  description = "Alert triggered when Used capacity exceeds 1%"
+  window_size       = "PT1H"
+}
